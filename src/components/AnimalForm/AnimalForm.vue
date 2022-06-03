adb2470ba0ff4d8f131a2485f42fe5b192322f45
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-03 23:54:41 +0300

    Created basic project structure

---
+++ src/components/AnimalForm/AnimalForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Form</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AnimalForm",
+});
+</script>
+
+<style scoped lang="scss" src="./AnimalForm.scss" />
---
+++ src/components/AnimalListContainer/AnimalListContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <AnimalSwitch />
+    <h1>Title</h1>
+    <AnimalForm />
+    <AnimalList />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AnimalForm from "@/components/AnimalForm/AnimalForm.vue";
+import AnimalSwitch from "@/components/AnimalSwitch/AnimalSwitch.vue";
+import AnimalList from "@/components/AnimalList/AnimalList.vue";
+
+export default defineComponent({
+  name: "AnimalListContainer",
+  components: {
+    AnimalSwitch,
+    AnimalForm,
+    AnimalList,
+  },
+});
+</script>
+
+<style scoped lang="scss" src="./AnimalListContainer.scss" />
